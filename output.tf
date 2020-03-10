c77004f04aa17974bcb4b2ea0850e03e79d98cde
Prashant Somashekar <prashsomash@gmail.com>
2020-03-10 07:03:23 -0400

    terraform: output hosts file for Ansible

--- output.tf
+++ output.tf
@@ -1,3 +1,10 @@
 output "static_public_ip" {
   value = azurerm_public_ip.external.ip_address
 }
+
+resource "local_file" "hosts" {
+  content              = "[vps]\n${azurerm_public_ip.external.ip_address} ansible_connection=ssh ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/${var.prefix} instance=${azurerm_virtual_machine.myweb.name} instance_rg=${azurerm_resource_group.myweb.name} instance_nsg=${azurerm_network_security_group.myweb.name}"
+  filename             = pathexpand("~/dev/ansible/hosts-azure")
+  directory_permission = 0754
+  file_permission      = 0664
+}
